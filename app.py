a605049c70fcd9b88b61bab3e8c49193a9541d01
hibikikkk <kyou9712@gmail.com>
2018-11-17 16:45:14 +0900

    FizzBuzz_convert関数をガード節を使ってみた。

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ def FizzBuzz_convert(number):
     if number % 15 == 0:
         return "FizzBuzz"
 
-    elif number % 5 == 0:
+    if number % 5 == 0:
         return "Buzz"
 
-    elif number % 3 == 0:
+    if number % 3 == 0:
         return "Fizz"
 
     return str(number)
